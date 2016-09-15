6cbaf27b5057a35a4a0dfdf6673c90d8947f61cc
scarletrain77 <chen_glassyuki@163.com>
2016-09-15 15:27:20 +0800

    还差输入

--- node/src/main.ts
+++ node/src/main.ts
@@ -1,21 +1,11 @@
-/*import http = require('http');
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
-console.log(`Server Started! Please visit http://127.0.0.1:${port}`);*/
 function pyramid (a:number):void{
     for(var  i:number = 0; i<a; i++){
-        //console.log('i:' + i);
         var b:string = '';
         for(var j:number = 0; j < a-i-1 ; j++){
             b += ' ';
-            //console.log('j:' + j);
         }
         for(var k:number = 0; k<2*i-1; k++){
             b += '*';
-            //console.log('k:' + k);
         }
         console.log(b + '\n');
     }
