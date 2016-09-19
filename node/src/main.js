f4ba9642b1d9920b4a519576d071333fa8a29ff6
scarletrain77 <chen_glassyuki@163.com>
2016-09-19 15:13:06 +0800

    Finish Homework 2

--- node/src/main.js
+++ node/src/main.js
@@ -1,31 +1,18 @@
-/*import http = require('http');
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
-console.log(`Server Started! Please visit http://127.0.0.1:${port}`);*/
+//var prompt = require('C:\Program Files (x86)\Egret\Egret Wing 3\resources\app\extensions\typescript\server\typescript\lib\lib.d.ts');
 function pyramid(a) {
-    for (var i = 0; i < a; i++) {
-        //console.log('i:' + i);
+    for (var i = 1; i <= a; i++) {
         var b = '';
-        for (var j = 0; j < a - i - 1; j++) {
+        for (var j = 0; j <= a - i - 1; j++) {
             b += ' ';
         }
-        for (var k = 0; k < 2 * i - 1; k++) {
+        for (var k = 1; k <= 2 * i - 1; k++) {
             b += '*';
         }
-        console.log(b + '\n');
+        console.log(b);
     }
 }
-//var numS:string = prompt("请输入一个数字");
+//var numS:string = prompt("请输入一个数字","");
+//var num:number = +numS;
+var numS = process.argv[2];
 //var num:number = parseInt(numS);
-var numS;
-process.stdin.on('readable', function () {
-    numS = process.stdin.read();
-    if (numS !== null) {
-        process.stdout.write("data: " + process);
-    }
-});
-var num = +numS;
-pyramid(num);
+pyramid(parseInt(numS));
