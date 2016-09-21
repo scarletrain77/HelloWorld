15c0ff462020d605f54e681310b8999d7077b19c
scarletrain77 <chen_glassyuki@163.com>
2016-09-21 19:10:13 +0800

    Finish Homework 2

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 //var prompt = require('C:\Program Files (x86)\Egret\Egret Wing 3\resources\app\extensions\typescript\server\typescript\lib\lib.d.ts');
-function pyramid (a:number):void{
-    for(var  i:number = 1; i <= a; i++){
-        var b:string = '';
-        for(var j:number = 0; j <= a-i-1 ; j++){
+function pyramid(a) {
+    for (var i = 1; i <= a; i++) {
+        var b = '';
+        for (var j = 0; j <= a - i - 1; j++) {
             b += ' ';
         }
-        for(var k:number = 1; k <= 2*i-1; k++){
+        for (var k = 1; k <= 2 * i - 1; k++) {
             b += '*';
         }
         console.log(b);
@@ -13,7 +13,6 @@ function pyramid (a:number):void{
 }
 //var numS:string = prompt("请输入一个数字","");
 //var num:number = +numS;
-
-var numS:string = process.argv[2];
+var numS = process.argv[2];
 //var num:number = parseInt(numS);
 pyramid(parseInt(numS));
